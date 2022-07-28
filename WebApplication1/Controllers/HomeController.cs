46649b449cbe81213b7bf64b48aec23760712eb2
AsiaDrew <asiadrew2@outlook.com>
2022-07-28 08:43:18 -0400

    first

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        MovieDAL movieDAL = new MovieDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MovieSearch()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieSearch(string title)
+        {
+            MovieModel result = movieDAL.GetMovie(title);
+            return View(result);
+        }
+
+        [HttpGet]
+        public IActionResult MovieNight()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieNight(string title1, string title2, string title3)
+        {
+            List<MovieModel> result = new List<MovieModel>
+            {
+                movieDAL.GetMovie(title1),
+                movieDAL.GetMovie(title2),
+                movieDAL.GetMovie(title3)
+            };
+            return View(result);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication1/Models/MovieDAL.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace WebApplication1.Models
+{
+    public class MovieDAL
+    {
+        public MovieModel GetMovie(string title)
+        {
+            string url = $"http://www.omdbapi.com/?apikey=53d42a9f&t={title}";
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+            MovieModel result = JsonConvert.DeserializeObject<MovieModel>(JSON);
+            return result;
+        }
+
+    }
+}
